13d41e5fa14faf2802b03b3cf3a2ec31ca7a3870
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-10-20 15:11:28 +0200

    added files

---
+++ script.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelectorAll(".nav-link-header");
+const navList = document.querySelectorAll(".nav-header");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navList.forEach(listItem => {
+      const children = listItem.firstElementChild;
+
+      if (children.classList.contains("active")) {
+        children.classList.remove("active");
+      }
+    });
+    link.classList.toggle("active");
+  });
+});
+
+// REMEMBER to change button hover colors
